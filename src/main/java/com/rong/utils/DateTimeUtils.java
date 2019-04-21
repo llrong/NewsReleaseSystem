cae0820e3f698f8acc2d82ec7e4d7e61c9e05a81
llrong <1191799097@qq.com>
2019-04-21 20:17:22 +0800

    second commit( service mapper dao pojo,MyBatis-generator)

---
+++ src/main/java/com/rong/utils/DateTimeUtils.java
@@ -0,0 +1,27 @@
+package com.rong.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class DateTimeUtils {
+    //将数据库中的时间戳转换成格式为"yyyy-MM-dd HH:mm:ss"的时间
+    public static String  getFormatTime(int time){
+        long t = (long)time*1000;
+
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//将毫秒级long值转换成日期格式
+
+        GregorianCalendar gc = new GregorianCalendar();
+
+        gc.setTimeInMillis(t);
+        String dateStr = dateformat.format(gc.getTime());
+        return  dateStr;
+    }
+    /**
+     * 获取当前的时间戳
+     *
+     * @return
+     */
+    public static int getCurrentTime() {
+        return (int) (System.currentTimeMillis() / 1000);
+    }
+}
