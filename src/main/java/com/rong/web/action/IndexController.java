a88aa0be22caacb69f2550a851e1faf8141c9154
llrong <1191799097@qq.com>
2019-05-20 23:24:46 +0800

    4：首页  数据返回

--- src/main/java/com/rong/web/action/IndexController.java
+++ src/main/java/com/rong/web/action/IndexController.java
@@ -1,19 +1,68 @@
 package com.rong.web.action;
 
+import com.rong.web.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @Controller
 public class IndexController {
     @RequestMapping("/login")
-    public String index() {
+    public String login() {
         return "login";
     }
     @RequestMapping("/register")
     public String register() {
         return "register";
     }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/addNews")
+    public String addNews() {
+        return "addNews";
+    }
+
+    @RequestMapping("/menu")
+    public String menu() {
+        return "menu";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @RequestMapping("/news")
+    public String news() {
+        return "news";
+    }
+
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=gb2312");
+        PrintWriter out = response.getWriter();
+        out.print("<script language=\"javascript\">confirm('确认退出登录？');window.location.href='/register'</script>");
+        return "login";
+    }
+
+    @RequestMapping("/hi")
+    public String index(Model model){
+//            String students ="刘洋";
+        User user = new User();
+        user.setPassword("jjjj");
+            model.addAttribute("s",user);
+            return "forget";
+    }
+
+
+
 }
